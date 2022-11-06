fa5d2f0c06137f86da85c51c96347e6a282c430a
hamed aljamry <hamed970@hotmail.com>
2022-11-06 19:05:45 +0000

    working on questions usuing js

---
+++ script.js
@@ -0,0 +1,16 @@
+const quizData = [
+  {
+    question: 'Who was the first president of the United States',
+    a: 'Hamed Aljamry',
+    b: 'George Washington',
+    c: 'John Adams',
+    d: 'James Madison'
+  }, {
+    question: 'Who invented the "Ruby" programming language?',
+    a: 'Yukihiro Matsumoto',
+    b: 'Lukas Musilek',
+    c: 'Tim Berners-Lee',
+    d: 'James Gosling'
+
+  }
+]
