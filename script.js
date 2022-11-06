ac1131dc943d188f9b11e4d7696672072ee45f67
hamed aljamry <hamed970@hotmail.com>
2022-11-06 20:41:21 +0000

    sorting out const names before applying them into function

--- script.js
+++ script.js
@@ -37,6 +37,20 @@ const quizData = [
   }
 ]
 
+const questionEl = document.getElementById('question');
 
 
-console.log("hello")
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+
+
+  currentQuiz += 1;
+}
