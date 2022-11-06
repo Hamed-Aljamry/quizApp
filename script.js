50d0b4125c7a4f738cf28bb7ef1741135f192e31
hamed aljamry <hamed970@hotmail.com>
2022-11-06 19:19:02 +0000

    questions done

--- script.js
+++ script.js
@@ -4,13 +4,35 @@ const quizData = [
     a: 'Hamed Aljamry',
     b: 'George Washington',
     c: 'John Adams',
-    d: 'James Madison'
+    d: 'James Madison',
+    correct: 'b'
   }, {
     question: 'Who invented the "Ruby" programming language?',
     a: 'Yukihiro Matsumoto',
     b: 'Lukas Musilek',
     c: 'Tim Berners-Lee',
-    d: 'James Gosling'
-
+    d: 'James Gosling',
+    correct: 'a'
+  }, {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Helicopters Terminals Motorboats Lambourghinis',
+    c: 'Hightext Markup Lines',
+    d: 'Hamburger Touches Mustard Love',
+    correct: 'a'
+  }, {
+    question: 'Who won the 2010 World Cup?',
+    a: 'Brazil🇧🇷',
+    b: 'Germany🇩🇪',
+    c: 'Sweden🇸🇪',
+    d: 'España🇪🇸',
+    correct: 'd'
+  }, {
+    question: 'What year was Javascript launched?',
+    a: '1993',
+    b: '1997',
+    c: '2003',
+    d: 'None of the above',
+    correct: 'd'
   }
 ]
